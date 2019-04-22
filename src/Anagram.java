223db6fbc8f80dd72c4fea8bd52f7490514ffc09
Mihail Sazonov <sazonov91@gmail.com>
2019-04-22 17:42:30 +0300

    Take out revers single word in single method

--- src/Anagram.java
+++ src/Anagram.java
@@ -2,41 +2,44 @@
 public class Anagram {
 
 	public String invert(String wordToReverse) {
-		String reversedStr = "";
 		String[] massToReverse = wordToReverse.split("\\p{Space}");
-
+		String readySting = "";
 		for (String word : massToReverse) {
-			String[] massLetters = word.split("");
-			int len = massLetters.length - 1;
-			int counter = massLetters.length - 1;
-			
-			for (int i = 0; i <= len / 2; i++) {
-				if (isLetter(massLetters[i])) {
-					if (isLetter(massLetters[counter])) {
-						String tmpLetter = massLetters[i];
-						massLetters[i] = massLetters[counter];
-						massLetters[counter] = tmpLetter;
-						counter--;
-					} else {
-						counter--;
-						i--;
-					}
-				}
-			}
-			for (int j = 0; j < massLetters.length; j++) {
-				reversedStr += massLetters[j];
-			}
-			reversedStr += " ";
+			readySting += reversWord(word) + " ";
 		}
-		reversedStr = reversedStr.substring(0, reversedStr.length() - 1);
-		return reversedStr;
+		readySting = readySting.substring(0, readySting.length()-1);
+		return readySting;
 	}
 
-	private static boolean isLetter(String substring) {
+	private boolean isLetter(String substring) {
 		if (substring.matches("\\p{javaLetter}")) {
 			return true;
 		}
 		return false;
 	}
 
+	private String reversWord(String word) {
+		String readyStr = "";
+		String[] massLetters = word.split("");
+		int len = massLetters.length - 1;
+		int counter = massLetters.length - 1;
+
+		for (int i = 0; i <= len / 2; i++) {
+			if (isLetter(massLetters[i])) {
+				if (isLetter(massLetters[counter])) {
+					String tmpLetter = massLetters[i];
+					massLetters[i] = massLetters[counter];
+					massLetters[counter] = tmpLetter;
+					counter--;
+				} else {
+					counter--;
+					i--;
+				}
+			}
+		}
+		for (int i = 0; i < massLetters.length; i++) {
+			readyStr += massLetters[i];
+		}
+		return readyStr;
+	}
 }
