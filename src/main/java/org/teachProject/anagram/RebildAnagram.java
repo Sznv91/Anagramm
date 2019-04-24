110489db08d828dc0ae7354cd1526919cc398bbd
Mihail Sazonov <sazonov91@gmail.com>
2019-04-24 22:17:51 +0300

    Add copy class Anagram for correction

---
+++ src/main/java/org/teachProject/anagram/RebildAnagram.java
@@ -0,0 +1,46 @@
+package org.teachProject.anagram;
+
+public class RebildAnagram {
+
+	public String reversAllWords(String wordToReverse) {
+		String[] massToReverse = wordToReverse.split("\\p{Space}");
+		String readySting = "";
+		for (String word : massToReverse) {
+			readySting += reversOneWord(word) + " ";
+		}
+		readySting = readySting.substring(0, readySting.length()-1);
+		return readySting;
+	}
+
+	private boolean isLetter(String substring) {
+		if (substring.matches("\\p{javaLetter}")) {
+			return true;
+		}
+		return false;
+	}
+
+	private String reversOneWord(String word) {
+		String readyStr = "";
+		String[] massLetters = word.split("");
+		int len = massLetters.length - 1;
+		int counter = massLetters.length - 1;
+
+		for (int i = 0; i <= len / 2; i++) {
+			if (isLetter(massLetters[i])) {
+				if (isLetter(massLetters[counter])) {
+					String tmpLetter = massLetters[i];
+					massLetters[i] = massLetters[counter];
+					massLetters[counter] = tmpLetter;
+					counter--;
+				} else {
+					counter--;
+					i--;
+				}
+			}
+		}
+		for (int i = 0; i < massLetters.length; i++) {
+			readyStr += massLetters[i];
+		}
+		return readyStr;
+	}
+}
