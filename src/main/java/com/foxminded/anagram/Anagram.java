d91b67f421b04070bb366080da828d0c90163ed4
Mihail Sazonov <sazonov91@gmail.com>
2019-04-25 15:15:45 +0300

    replaced cycle "for" on "while"

--- src/main/java/com/foxminded/anagram/Anagram.java
+++ src/main/java/com/foxminded/anagram/Anagram.java
@@ -1,14 +1,15 @@
 package com.foxminded.anagram;
 
-import java.util.Arrays;
-
 public class Anagram {
 
 	public String reverseText(String text) {
 		String[] words = text.split(" ");
 		StringBuilder result = new StringBuilder();
 		for (String word : words) {
-			result.append(reversWord(word));
+			if (word.equals(words[words.length - 1])) {
+				result.append(reversWord(word));
+			} else
+				result.append(reversWord(word) + " ");
 		}
 		return result.toString();
 	}
@@ -16,23 +17,23 @@ public class Anagram {
 	private String reversWord(String word) {
 		String readyStr;
 		char[] symbols = word.toCharArray();
-		int len = symbols.length - 1;
-		int counter = symbols.length - 1;
 
-		for (int i = 0; i <= len / 2; i++) {
-			if (Character.isLetter(symbols[i])) {
-				if (Character.isLetter(symbols[counter])) {
-					char tmpLetter = symbols[i];
-					symbols[i] = symbols[counter];
-					symbols[counter] = tmpLetter;
-					counter--;
-				} else {
-					counter--;
-					i--;
-				}
-			}
+		int rightSymbol = symbols.length - 1;
+		int leftSymbol = 0;
+		while (rightSymbol > (symbols.length - 1) / 2) {
+			if (Character.isLetter(symbols[leftSymbol])) {
+				if (Character.isLetter(symbols[rightSymbol])) {
+					char tmpLetter = symbols[leftSymbol];
+					symbols[leftSymbol] = symbols[rightSymbol];
+					symbols[rightSymbol] = tmpLetter;
+					leftSymbol++;
+					rightSymbol--;
+				} else
+					rightSymbol--;
+			} else
+				leftSymbol++;
 		}
-		readyStr = Arrays.toString(symbols);
+		readyStr = new String(symbols);
 		return readyStr;
 	}
 }
