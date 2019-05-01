3353692dc9bb08e802cf645fe37f00b8adb14082
Sznv91 <47347643+Sznv91@users.noreply.github.com>
2019-05-01 15:51:05 +0300

    ready

--- src/main/java/com/foxminded/anagram/Anagram.java
+++ src/main/java/com/foxminded/anagram/Anagram.java
@@ -2,32 +2,23 @@ package com.foxminded.anagram;
 
 import static java.lang.Character.isLetter;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-
 public class Anagram {
 
-	private static ArrayList<String> storage = new ArrayList<>();
 	public final String SPACE = " ";
-	
+
 	public String reverseText(String text) {
 		String[] words = text.split(SPACE);
 		StringBuilder result = new StringBuilder();
-		Arrays.stream(words).forEach(word -> reversWord(word));
-		Iterator<String> iterator = storage.iterator();
-		while (true) {
-			result.append(iterator.next());
-			if (iterator.hasNext()) {
+		for (int i = 0; i < words.length; i++) {
+			result.append(reversWord(words[i]));
+			if (i != words.length - 1) {
 				result.append(SPACE);
-			} else {
-				break;
 			}
 		}
 		return result.toString();
 	}
 
-	private void reversWord(String word) {
+	private String reversWord(String word) {
 		char[] chars = word.toCharArray();
 		int rightIndex = chars.length - 1;
 		int leftIndex = 0;
@@ -46,6 +37,6 @@ public class Anagram {
 				leftIndex++;
 			}
 		}
-		storage.add(new String(chars));
+		return new String(chars);
 	}
 }
